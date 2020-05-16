0171e17d044e42b917d2bf3e4488b2d2ae402683
Alec Hall <alecnhall@gmail.com>
2020-05-16 14:03:34 -0600

    list component added

--- app.js
+++ app.js
@@ -1,5 +1,108 @@
-ReactDOM.render(React.createElement(
-    'h1',
-    null,
-    'Hello, world'
-), document.getElementById('root'));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Todo = function (_React$Component) {
+  _inherits(Todo, _React$Component);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    var _this = _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+
+    _this.handleChange = function (event) {
+      _this.setState({
+        input: event.target.value
+      });
+      console.log(_this.state.input);
+    };
+
+    _this.handleCheck = function (event) {
+      _this.setState({
+        done: event.target.done
+      });
+      console.log(_this.state.done);
+    };
+
+    _this.state = {
+      done: _this.props.done,
+      input: _this.props.input
+    };
+    return _this;
+  }
+
+  _createClass(Todo, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "todo" },
+        React.createElement("input", {
+          type: "checkbox",
+          name: "checkbox",
+          id: "checkbox",
+          done: this.state.done,
+          onChange: this.handleCheck
+        }),
+        React.createElement("input", {
+          type: "text",
+          name: "input",
+          id: "input",
+          value: this.state.input,
+          onChange: this.handleChange
+        })
+      );
+    }
+  }]);
+
+  return Todo;
+}(React.Component);
+
+var TodoList = function (_React$Component2) {
+  _inherits(TodoList, _React$Component2);
+
+  function TodoList(props) {
+    _classCallCheck(this, TodoList);
+
+    var _this2 = _possibleConstructorReturn(this, (TodoList.__proto__ || Object.getPrototypeOf(TodoList)).call(this, props));
+
+    _this2.state = {
+      todos: [{ _id: 1, input: "Item 1", done: false }, { _id: 2, input: "Item 2", done: false }, { _id: 3, input: "Item 3", done: false }, { _id: 4, input: "Item 4", done: false }]
+    };
+    return _this2;
+  }
+
+  _createClass(TodoList, [{
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      return React.createElement(
+        "div",
+        { className: "todo-list" },
+        React.createElement(
+          "h1",
+          null,
+          "todoList"
+        ),
+        this.state.todos.map(function (todo) {
+          React.createElement(Todo, {
+            key: todo._id.toString(),
+            id: todo.id,
+            input: todo.input,
+            done: todo.done
+          });
+          console.log(_this3.state.todos);
+        })
+      );
+    }
+  }]);
+
+  return TodoList;
+}(React.Component);
+
+ReactDOM.render(React.createElement(TodoList, null), document.getElementById("root"));
--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,78 @@
-ReactDOM.render(
-    <h1>Hello, world</h1>,
-    document.getElementById('root')
-  );
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: this.props.done,
+      input: this.props.input,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+    console.log(this.state.input);
+  };
+
+  handleCheck = (event) => {
+    this.setState({
+      done: event.target.done,
+    });
+    console.log(this.state.done);
+  };
+
+  render() {
+    return (
+      <div className="todo">
+        <input
+          type="checkbox"
+          name="checkbox"
+          id="checkbox"
+          done={this.state.done}
+          onChange={this.handleCheck}
+        />
+        <input
+          type="text"
+          name="input"
+          id="input"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { _id: 1, input: "Item 1", done: false },
+        { _id: 2, input: "Item 2", done: false },
+        { _id: 3, input: "Item 3", done: false },
+        { _id: 4, input: "Item 4", done: false },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        <h1>todoList</h1>
+        {this.state.todos.map((todo) => {
+          <Todo
+            key={todo._id.toString()}
+            id={todo.id}
+            input={todo.input}
+            done={todo.done}
+          />;
+          console.log(this.state.todos)
+        })}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById("root"));
